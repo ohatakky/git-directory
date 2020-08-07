4a90374260b8b5d46d7e84ccc2359f570aca0e31
Akira Otaki <ohatakky@gmail.com>
2020-08-08 02:29:41 +0900

    fix

--- ui/src/components/pages/Tree/index.tsx
+++ ui/src/components/pages/Tree/index.tsx
@@ -2,27 +2,31 @@ import React, { FC, Fragment, useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
 import { WS_API_HOST } from "~/utils/constants";
 
+type Tree = {
+  i: number;
+  hash: string;
+  t: string[];
+};
+
 const actionTypes = {
   success: "success",
-  reset: "reset",
+  failed: "failed",
 } as const;
 type ActionType = typeof actionTypes[keyof typeof actionTypes];
 
 const reducer = (
-  state: string[],
-  action: { type: ActionType; payload: string },
+  state: Tree[],
+  action: { type: ActionType; payload: Tree },
 ) => {
   switch (action.type) {
     case actionTypes.success:
       return [...state, action.payload];
-    case actionTypes.reset:
+    case actionTypes.failed:
       return [];
-    default:
-      throw new Error("Unexpected action");
   }
 };
 
-const Tree: FC = () => {
+const TreeFC: FC = () => {
   const { org, repo } = useParams();
 
   const [trees, dispatch] = useReducer(reducer, []);
@@ -30,10 +34,12 @@ const Tree: FC = () => {
     const ws = new WebSocket(`${WS_API_HOST}/ws?repo=${org}/${repo}`);
     ws.onmessage = ({ data }) => {
       try {
-        const packet = JSON.parse(data);
+        const packet = JSON.parse(data) as Tree;
         dispatch({ type: actionTypes.success, payload: packet });
       } catch (e) {
-        return;
+        dispatch(
+          { type: actionTypes.failed, payload: { i: 0, hash: "", t: [] } },
+        );
       }
     };
     return (): void => {
@@ -43,13 +49,13 @@ const Tree: FC = () => {
     };
   }, []);
 
-  console.log(trees);
-
   return (
     <Fragment>
-      <Fragment></Fragment>
+      {trees.map((tree) => (
+        <div>{tree.hash}</div>
+      ))}
     </Fragment>
   );
 };
 
-export default Tree;
+export default TreeFC;
