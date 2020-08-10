5b992e3fe99a0070a357ef66f9253aceadc931d4
Akira Otaki <ohatakky@gmail.com>
2020-08-10 16:40:22 +0900

    add:progress_chart

--- ui/src/components/pages/Tree/index.tsx
+++ ui/src/components/pages/Tree/index.tsx
@@ -1,8 +1,9 @@
 import React, { FC, Fragment, useState, useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
 import { WS_API_HOST } from "~/utils/constants";
+import { LinearProgress } from "@material-ui/core";
 
-type Tree = {
+type Fzf = {
   hash: string;
   files: string[];
   commit: {
@@ -18,8 +19,8 @@ const actionTypes = {
 type ActionType = typeof actionTypes[keyof typeof actionTypes];
 
 const reducer = (
-  state: Tree[],
-  action: { type: ActionType; payload: Tree },
+  state: Fzf[],
+  action: { type: ActionType; payload: Fzf },
 ) => {
   switch (action.type) {
     case actionTypes.success:
@@ -29,15 +30,15 @@ const reducer = (
   }
 };
 
-const TreeFC: FC = () => {
+const FzfFC: FC = () => {
   const { org, repo } = useParams();
-  const [trees, dispatch] = useReducer(reducer, []);
+  const [fzfs, dispatch] = useReducer(reducer, []);
   const [idx, setIdx] = useState(0);
   useEffect(() => {
     const ws = new WebSocket(`${WS_API_HOST}/ws?repo=${org}/${repo}`);
     ws.onmessage = ({ data }) => {
       try {
-        const packet = JSON.parse(data) as Tree;
+        const packet = JSON.parse(data) as Fzf;
         dispatch({ type: actionTypes.success, payload: packet });
       } catch (e) {
         dispatch({
@@ -58,7 +59,7 @@ const TreeFC: FC = () => {
       if (e.keyCode === 37) {
         setIdx((i) => (i <= 0 ? i : i - 1));
       } else if (e.keyCode === 39) {
-        setIdx((i) => (i >= trees.length - 1 ? i : i + 1));
+        setIdx((i) => (i >= fzfs.length - 1 ? i : i + 1));
       }
     };
     document.addEventListener("keydown", onKeyDown);
@@ -66,32 +67,62 @@ const TreeFC: FC = () => {
     return () => {
       document.removeEventListener("keydown", onKeyDown);
     };
-  }, [trees.length]);
+  }, [fzfs.length]);
+
+  // todo: terminal view
+  // @ ~/git-directory/gorilla/websocket master ()
+  // $ tree
+  // author
+  // PR
 
   // todo: loading
-  // todo: author
-  // todo: PR
-
   // todo: fuzzy finder表示
   // todo: tree表示
 
   return (
     <div style={{ width: "100%", height: "100%", backgroundColor: "#002b36" }}>
-      {trees.length > 0 && (
-        <Fragment>
-          <div style={{ color: "859900", fontSize: 10, fontFamily: "Monaco" }}>
-            {trees[idx].hash}
+      {fzfs.length > 0
+        ? (
+          <div>
+            <div style={{ display: "flex" }}>
+              <div
+                style={{ color: "#657b83", fontSize: 10, fontFamily: "Monaco" }}
+              >
+                {`@ ~/git-directory/${org}/${repo}`}
+              </div>
+              <div
+                style={{ color: "#859900", fontSize: 10, fontFamily: "Monaco" }}
+              >
+                {fzfs[idx].hash}
+              </div>
+              <div
+                style={{ color: "#268bd2", fontSize: 10, fontFamily: "Monaco" }}
+              >
+                {fzfs[idx].commit.author}
+              </div>
+            </div>
+
+            <div
+              style={{ color: "#eee8d5", fontSize: 10, fontFamily: "Monaco" }}
+            >
+              {fzfs[idx].commit.message}
+            </div>
+
+            <div
+              style={{ color: "#eee8d5", fontSize: 10, fontFamily: "Monaco" }}
+            >
+              <div>$ fzf</div>
+              {fzfs[idx].files.map((f, i) => (
+                <div>{f}</div>
+              ))}
+            </div>
           </div>
-          <div style={{ color: "#eee8d5", fontSize: 10, fontFamily: "Monaco" }}>
-            <div>tree</div>
-            {trees[idx].files.map((f, i) => (
-              <div>{f}</div>
-            ))}
-          </div>
-        </Fragment>
-      )}
+        )
+        : (
+          <LinearProgress />
+        )}
     </div>
   );
 };
 
-export default TreeFC;
+export default FzfFC;
