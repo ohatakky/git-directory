1cf6dbecac09752e0a3fab32382d7f47d6bee1c7
Akira Otaki <ohatakky@gmail.com>
2020-08-14 18:23:50 +0900

    deploy for websocket & appengine

--- server/exec.sh
+++ server/exec.sh
@@ -2,6 +2,13 @@ function run() {
     watchexec -r go run main.go
 }
 
+function pre() {
+    go mod vendor &&
+    cp -r ./ $GOPATH/src/github.com/ohatakky/git-directory/server
+    rm -f $GOPATH/src/github.com/ohatakky/git-directory/server/go.sum
+    rm -f $GOPATH/src/github.com/ohatakky/git-directory/server/go.mod
+}
+
 function deploy() {
     gcloud app deploy
 }
